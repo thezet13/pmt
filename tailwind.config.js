c13c3efe8b606c7dc62fd5296c625c66a91adec5
Roman <thezet@gmail.com>
2026-05-20 10:56:25 +0400

    Fix Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
 
     theme: {
